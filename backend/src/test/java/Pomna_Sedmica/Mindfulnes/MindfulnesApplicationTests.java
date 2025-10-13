c15752ae7cc2608a138ad345e368c913ab80a139
Martin Ante Rogošić <martin-ante.rogosic@fer.hr>
2025-10-13 17:12:23 +0000

    Početna konfiguracija repozitorija i radnih okvira

---
+++ backend/src/test/java/Pomna_Sedmica/Mindfulnes/MindfulnesApplicationTests.java
@@ -0,0 +1,13 @@
+package Pomna_Sedmica.Mindfulnes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindfulnesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
