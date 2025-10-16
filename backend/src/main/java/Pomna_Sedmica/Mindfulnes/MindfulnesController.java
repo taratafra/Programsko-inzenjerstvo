d73122030bd3685122f4e4d1db78250490697fe8
taratafra <tara.tafra@fer.hr>
2025-10-16 15:23:51 +0200

    Dodaj kontroler datoteku

---
+++ backend/src/main/java/Pomna_Sedmica/Mindfulnes/MindfulnesController.java
@@ -0,0 +1,15 @@
+package Pomna_Sedmica.Mindfulnes;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000") // allow React (frontend) to access this
+public class MindfulnesController {
+
+    @GetMapping("/api/connect")
+    public String connectMessage() {
+        return "You are officially connected!";
+    }
+}
