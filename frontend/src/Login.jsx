f01c0937d04a4995779d7557571f3f7b22a2d3ea
MartinAnteRogosic <148678674+MartinAnteRogosic@users.noreply.github.com>
2025-11-14 17:47:30 +0100

    Update Login.jsx (#47)

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -4,6 +4,7 @@ import CloudBackground from "./components/backgrounds/CloudyBackground";
 import WhiteRectangle from "./components/backgrounds/WhiteRectangle.jsx";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import { FcGoogle } from "react-icons/fc";
 
 function Login() {
   const [email, setEmail] = useState("");
