3c30bd70256bd3689ee529cb325d402dfd074ea5
Tara Tafra <tara.tafra@fer.hr>
2026-01-13 15:08:52 +0100

    sitna promjena

--- backend/src/main/java/Pomna_Sedmica/Mindfulnes/controller/UserController.java
+++ backend/src/main/java/Pomna_Sedmica/Mindfulnes/controller/UserController.java
@@ -86,6 +86,7 @@
 //}
 package Pomna_Sedmica.Mindfulnes.controller;
 
+import Pomna_Sedmica.Mindfulnes.domain.dto.SaveAuth0UserRequestDTO;
 import Pomna_Sedmica.Mindfulnes.domain.dto.UserDTOResponse;
 import Pomna_Sedmica.Mindfulnes.domain.entity.User;
 import Pomna_Sedmica.Mindfulnes.mapper.UserMapper;
@@ -161,5 +162,11 @@ public class UserController {
         return userService.completeOnboardingByAuth0Id(user.getAuth0Id())
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTOResponse> saveOrUpdateUser(@RequestBody SaveAuth0UserRequestDTO request) {
+        UserDTOResponse savedUser = userService.saveOrUpdateUser(request);
+        return ResponseEntity.ok(savedUser);
     }
 }
