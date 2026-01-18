b10c3ea2522a0be545191d0535257ca0531faa33
IvLucic <ivan.lucic@fer.unizg.hr>
2026-01-18 18:42:15 +0100

    Added first submission badge

--- backend/src/main/java/Pomna_Sedmica/Mindfulnes/domain/enums/BadgeType.java
+++ backend/src/main/java/Pomna_Sedmica/Mindfulnes/domain/enums/BadgeType.java
@@ -1,6 +1,8 @@
 package Pomna_Sedmica.Mindfulnes.domain.enums;
 
 public enum BadgeType {
+    FIRST_SUBMISSION,   // NEW: prvi put kad korisnik zavrsi DailyFocus
+
     STREAK_7,
     STREAK_30,
     STREAK_100
