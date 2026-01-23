7b7916236025a88c6196f6e6908831746ae52d05
MartinAnteRogosic <148678674+MartinAnteRogosic@users.noreply.github.com>
2026-01-23 21:31:13 +0100

    Fix deploy (#83)
    * Fixed audience wierdens
    
    * Added missing imports

--- frontend/src/components/home/tabPanel/DailyFocus/Exercise.jsx
+++ frontend/src/components/home/tabPanel/DailyFocus/Exercise.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./MoodHabits.module.css";
 
 export default function DailyExerciseVideo() {
